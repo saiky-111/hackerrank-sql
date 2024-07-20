01ff616b6889d580656fda760f1f7d4079ba1372
Saikat Dhibar <saikat111dhibar@gmail.com>
2024-07-20 10:52:28 +0530

    new commit

--- SQL/Higher_than_75_marks.sql
+++ SQL/Higher_than_75_marks.sql
@@ -1,1 +1,12 @@
 select NAME from STUDENTS where Marks > 75 order by substring(NAME,-3), ID;
+
+#Solution 1:
+SELECT Name
+FROM STUDENTS
+WHERE MARKS > 75
+ORDER BY RIGHT(NAME, 3) ASC, ID;
+
+#Solution 2:
+SELECT name FROM students
+WHERE marks > 75
+ORDER BY SUBSTR(name, LENGTH(name)-2, 3), id;
