f46f685297aa148b6346d07fb66f05444f1add5c
Saikat Dhibar <saikat111dhibar@gmail.com>
2024-07-22 22:13:45 +0530

    new commit

--- SQL/japanese_cities_names.sql
+++ SQL/japanese_cities_names.sql
@@ -1,1 +1,22 @@
+Q. Japanese Cities Names | Easy | HackerRank
+
+Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN.
+
+The CITY table is described as follows:
+
+CITY
+----------------------------------
+| Field       | Type            |
+|-------------|------------------|
+| ID          | NUMBER           |
+| NAME        | VARCHAR2(17)     |
+| COUNTRYCODE | VARCHAR2(3)      |
+| DISTRICT    | VARCHAR2(20)     |
+| POPULATION  | NUMBER           |
+----------------------------------
+
+
+Solution:
+
+
 SELECT NAME FROM CITY WHERE COUNTRYCODE = 'JPN';
